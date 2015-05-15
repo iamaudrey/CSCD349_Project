2ae46d18b959b4b2b1a9c64987c39a039df2cf14
iamaudrey <greataudrey@gmail.com>
2015-05-14 19:25:54 -0700

    I don't know exactly what I'm doing. :|

---
+++ AbilitiesProject_rev000/AbilitiesProject_rev000/SmallHealthPotion.cs
@@ -0,0 +1,25 @@
+﻿//SmallHealthPotion.cs
+//Audrey Henry
+
+using System;
+
+namespace CSCD349Project
+{
+    public class SmallHealthPotion : GameItem
+    {
+        //Default constructor for SmallHealthPotion
+        public SmallHealthPotion()
+        {
+            SetName("Small Health Potion");
+        }
+
+        public override void Use(GameCharacter character)
+        {
+            //We can adjust the values to achieve a better balance. AH
+            character.GetAttributes()._health += 50.0;
+
+        }
+    }
+
+}
+
